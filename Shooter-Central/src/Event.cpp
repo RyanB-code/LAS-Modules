445561b886d5b793758a0db06021d4a05009ed43
Bradley Ryan <ryan.bradley.e@gmail.com>
2024-10-09 17:50:56 -0700

    Amended match event object to be USPSA_MATCH. Added event tracker and hasing of Event objects

--- Shooter-Central/src/Event.cpp
+++ Shooter-Central/src/Event.cpp
@@ -26,11 +26,13 @@ std::ostream& operator<<(std::ostream& stream, const EventType& event){
     return stream;
 }
 
-Event::Event(std::string setName, EventLocation setLocation, EventType setEventType, std::string setNotes)
+// MARK: EVENT
+Event::Event(std::string setName, std::string setLocation, EventType setEventType, std::string setNotes, Datestamp setDate)
     :   name        { setName },
         location    { setLocation },
         eventType   { setEventType },
-        notes       { setNotes }
+        notes       { setNotes },
+        date        { setDate }
 {
 
 }
@@ -46,19 +48,75 @@ std::string Event::getNotes() const{
 EventType Event::getEventType() const{
     return eventType;
 }
-EventLocation Event::getEventLocation() const{
+std::string Event::getLocation() const{
     return location;
 }
-/*
-bool Event::addGun(GunPtr gun, AmmoPtr ammo, uint64_t rounds){
-    if(!gun || !ammo || rounds == 0)
+const Datestamp& Event::getDatestamp() const{
+    return date;
+}
+bool Event::addGun(GunPtr gun, TrackedAmmo ammoUsed){
+    if(!gun)
         return false;
+
+    uint8_t gunsInList { 0 };
+    for(const auto& pair : gunsUsed){
+        if(pair.first != nullptr)
+            ++gunsInList;
+    }
+
+    if(gunsInList >= gunsUsed.max_size())
+        return false;
+
+    // Add gun
+    gunsUsed[gunsInList-1] = std::pair(gun, ammoUsed);
     
     // Add to round counter for gun
-    if(!gun->addToRoundCount(ammo, rounds))
+    if(!gun->addToRoundCount(ammoUsed.amount, ammoUsed.ammoType))
         return false;
     
-    gunsUsed.push_back(std::tuple{gun, ammo, rounds});
     return true;
 }
-*/
+bool Event::operator==(const Event& other) const{
+    if(this->name == other.getName() && this->location == other.getLocation() && this->date.printDate() == other.getDatestamp().printDate())
+        return true;
+
+    return false;
+}
+
+
+// MARK: EVENT TRACKER
+EventTracker::EventTracker(LAS::Logging::LoggerPtr setLogger) : logger { setLogger }
+{
+
+}
+EventTracker::~EventTracker()
+{
+
+}
+bool EventTracker::setDirectory(std::string path) {
+    path = LAS::TextManip::ensureSlash(path);
+
+    if(std::filesystem::exists(path)){
+        saveDirectory = path;
+        return true;
+    }
+    else
+        return false;
+}
+std::string EventTracker::getDirectory() const{
+    return saveDirectory;
+}
+bool EventTracker::addEvent (Event& event){
+   if(events.contains(event))
+        return false;
+
+    return events.try_emplace(event, std::make_shared<Event>(event)).second;
+}
+
+void EventTracker::getAllEvents(std::vector<Event>& list) const {
+    if(!list.empty())
+        list.erase(list.begin(), list.end());
+
+    for(const auto& [key, event] : events)
+        list.push_back(*event);
+}
--- Shooter-Central/src/Event.h
+++ Shooter-Central/src/Event.h
@@ -2,6 +2,7 @@
 
 #include "Gun.h"
 #include "Ammo.h"
+#include "Time.h"
 
 #include <stdint.h>
 #include <string>
@@ -27,37 +28,75 @@ namespace ShooterCentral{
     };
 
     namespace EventTypes{
-        const EventType MATCH           { "MATCH" };
+        const EventType USPSA_MATCH     { "USPSA MATCH" };
         const EventType WORK_RANGE      { "WORK RANGE" };
         const EventType PERSONAL_RANGE  { "PERSONAL RANGE" };
         const EventType ET_NA           { "N/A" };
     }
 
-    struct EventLocation{
-        std::string location;
-    };
-
     // MARK: MATCH INFO
     class Event final {
     public:
-        Event(std::string setName, EventLocation setLocation, EventType setEventType, std::string setNotes);
+        Event(std::string setName, std::string setLocation, EventType setEventType, std::string setNotes, Datestamp setDate);
         ~Event();
 
-        std::string     getName()           const;
-        std::string     getNotes()          const;
-        EventType       getEventType()      const;
-        EventLocation   getEventLocation()  const;
+        std::string         getName()           const;
+        std::string         getNotes()          const;
+        EventType           getEventType()      const;
+        std::string         getLocation()       const;
+        const Datestamp&    getDatestamp()      const;
 
-       // bool            addGun(GunPtr gun, AmmoPtr ammo, uint64_t rounds);
+        bool addGun(GunPtr gun, TrackedAmmo ammoUsed);
 
+        bool operator==(const Event& other) const;
     private:
         std::string                                         name;
-        EventLocation                                       location;
+        std::string                                         location;
         EventType                                           eventType;
-       // std::vector<std::tuple<GunPtr, AmmoPtr, uint64_t>>  gunsUsed;
+        std::array<std::pair<GunPtr, TrackedAmmo>, 5>       gunsUsed;
         std::string                                         notes;
-        
+        Datestamp                                           date;
     };
-
     using EventPtr = std::shared_ptr<Event>;
+}
+
+namespace std{
+    template <>
+    struct hash<ShooterCentral::Event> {
+        size_t operator()(const ShooterCentral::Event& event) const{
+            std::size_t seed { 0 };
+
+            std::hash_combine(seed, event.getName());
+            std::hash_combine(seed, event.getLocation());
+            std::hash_combine(seed, event.getDatestamp().printDate());
+
+            return seed;
+        }
+    };
+}
+
+namespace ShooterCentral{
+
+    class EventTracker {
+    public:
+        EventTracker(LAS::Logging::LoggerPtr setLogger);
+        ~EventTracker();
+
+        bool        setDirectory    (std::string directory);
+        std::string getDirectory    () const;
+
+        bool addEvent (Event& event);
+
+        void        getAllEvents  (std::vector<Event>& list)    const;  // Clears vector before adding elements
+
+    private:
+        std::unordered_map<Event, EventPtr>    events;
+
+        std::string             saveDirectory;
+        LAS::Logging::LoggerPtr logger;
+
+        static constexpr std::string CARTRIDGES_FILENAME {"Cartridges.json"};
+    };
+    using EventTrackerPtr = std::shared_ptr<EventTracker>;
+
 }
