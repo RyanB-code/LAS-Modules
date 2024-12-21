7bdafb7efb8da91bb011f6309a879b5e56547af3
Bradley Ryan <ryan.bradley.e@gmail.com>
2024-12-21 13:47:07 -0800

    Added type for WeaponType
    Added hashing of WeaponType. Updated member functions to use the new type.

--- Shooter-Central/src/Gun.h
+++ Shooter-Central/src/Gun.h
@@ -9,16 +9,34 @@
 
 //MARK: GUN
 namespace ShooterCentral{
+
+    class WeaponType{
+    public:
+        explicit WeaponType(std::string setName="NULL");
+        ~WeaponType();
+
+        std::string getName() const;
+        operator std::string() const;
+        bool operator==(const WeaponType& other) const;
+
+    private:
+        std::string name;
+    };
+
+    std::ostream& operator<<(std::ostream& os, const WeaponType& weaponType);
+
+    using WeaponTypeList = std::vector<WeaponType>;
+
     class Gun final {
     public:
         Gun();
-        Gun(std::string setName, std::string setWeaponType, std::string setCartridge);
+        Gun(std::string setName, WeaponType setWeaponType, Cartridge setCartridge);
         ~Gun();
 
         std::string getName         () const;
         uint64_t    getRoundCount   () const;
-        std::string getWeaponType   () const;
-        std::string getCartridge    () const;
+        WeaponType  getWeaponType   () const;
+        Cartridge   getCartridge    () const;
 
         bool addToRoundCount (int amount, const AmmoType& ammoType);
 
@@ -28,8 +46,8 @@ namespace ShooterCentral{
 
     private:
         std::string name;
-        std::string weaponType;
-        std::string cartridge;
+        WeaponType  weaponType;
+        Cartridge   cartridge;
 
         std::unordered_map<AmmoType, TrackedAmmoPtr> ammoTracker;
     };
@@ -38,6 +56,12 @@ namespace ShooterCentral{
     void from_json  (const LAS::json& j, Gun& gun); 
 }
 namespace std{
+    template <>
+    struct hash<ShooterCentral::WeaponType> {
+        size_t operator()(const ShooterCentral::WeaponType& wt) const{
+            return std::hash<std::string>()(wt.getName());
+        }
+    };
     template <>
     struct hash<ShooterCentral::Gun> {
         size_t operator()(const ShooterCentral::Gun& gun) const{
@@ -65,27 +89,27 @@ namespace ShooterCentral{
         bool        setDirectory    (std::string directory);
         std::string getDirectory    () const;
 
-        GunPtr  createGun (const std::string& name, const std::string& weaponType, const std::string& cartridge);
-        bool    removeGun (const Gun& gun);
+        GunPtr  createGun       (const std::string& name, const WeaponType& weaponType, const Cartridge& cartridge);
+        bool    removeGun       (const Gun& gun);
 
-        void    getRoundsShotPerCartridge   (std::unordered_map<std::string, uint64_t>& list) const;
+        bool    addWeaponType   (const std::string& type);
 
-        uint64_t    getGunTotal () const;
-        void        getAllGuns  (std::vector<ConstGunPtr>& list) const;      // Clears vector before adding elements 
 
-        void    getAllWeaponTypeNames   (StringVector& names) const;   // Clears vector before adding elements
-        bool    addWeaponType           (const std::string& type);
+        void    getRoundsShotPerCartridge   (std::unordered_map<std::string, uint64_t>& list)   const;  // Clears vector before adding elements
+        void    getAllWeaponTypeNames       (WeaponTypeList& names)                             const;  // Clears vector before adding elements
+        int     getGunTotal                 ()                                                  const;
+        void    getAllGuns                  (std::vector<ConstGunPtr>& list)                    const;  // Clears vector before adding elements 
 
-        bool writeAllGuns   () const;
-        bool readGuns       ();
+        bool writeAllGuns           () const;
+        bool writeAllWeaponTypes    () const;
 
-        bool writeAllWeaponTypes () const;
-        bool readWeaponTypes  ();
+        bool readGuns           ();
+        bool readWeaponTypes    ();
 
     private:
         LAS::Logging::LoggerPtr             logger;
         std::unordered_map<Gun, GunPtr>     guns;
-        std::map<std::string, std::string>  weaponTypes;
+        std::map<std::string, WeaponType>   weaponTypes;
 
         std::string saveDirectory;
 
@@ -95,11 +119,12 @@ namespace ShooterCentral{
     };
 
     namespace GunHelper{
-        bool    writeGun    (std::string directory, const Gun& gun);
-        Gun     readGun     (const std::string& path);
+        bool    writeGun            (std::string directory, const Gun& gun);
+        bool    writeAllWeaponTypes (std::string path,      const WeaponTypeList& list); 
 
-        bool    writeAllWeaponTypes      (std::string path, const std::vector<std::string>& list);
-        bool    readWeaponTypes          (std::string path, StringVector& list);                    // Clears vector before adding elements
+        bool    readWeaponTypes     (std::string path, WeaponTypeList& list);       // Clears vector before adding elements
+        Gun     readGun             (const std::string& path);
+
     }
 
     using GunTrackerPtr = std::shared_ptr<GunTracker>;
