a09ca970a82050812a07108e161a0f5d953a4927
Bradley Ryan <59941443+RyanB-code@users.noreply.github.com>
2024-12-28 17:41:29 -0800

    Added build scripts and CMakeLists file to help build process

---
+++ Shooter-Central/build/scripts/GetDebugFiles.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Input LAS parent directory location into the file
+# with a backslash at the end
+# DO NOT change the variable in this script unless that file name changes
+LAS_LOCATION_FILE=las-directory.dat
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )     # Where script is (should be Garage/build/scripts/
+LAS_DIR=$(cat ${SCRIPT_DIR}/${LAS_LOCATION_FILE})                                   # LAS directory
+
+cd ${SCRIPT_DIR}
+
+cp -r ${LAS_DIR}bin/SDK/Debug/include ../../    # Get header Files
+cp ${LAS_DIR}bin/SDK/Debug/libLAS-SDK-dev.so ../../lib/    # Get LAS-SDK-dev.so for compilation
