827b32bd17986e0768ca447c0ecea4b5177c7788
Bradley Ryan <ryan.bradley.e@gmail.com>
2025-02-02 14:45:31 -0800

    Updated to v0.3.2

--- Shooter-Central/src/Assembler.cpp
+++ Shooter-Central/src/Assembler.cpp
@@ -10,7 +10,7 @@ bool LASM_loadModuleInfo(LAS::Information::ModuleInfo& moduleInfo){
     Information::Version sdkVersion { SDK::getVersionMajor(), SDK::getVersionMinor(), SDK::getVersionPatch()};
 
     moduleInfo.sdkVersion       = sdkVersion;
-    moduleInfo.moduleVersion    = Information::Version{0, 3 , 0};
+    moduleInfo.moduleVersion    = Information::Version{0, 3 , 2};
     moduleInfo.title            = application.getWindow()->getTitle();
     moduleInfo.commandGroupName = application.getCommandGroupName();
     moduleInfo.window           = application.getWindow();
