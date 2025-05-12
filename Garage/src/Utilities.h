b5f02c28db0c3187ff218980f05136ef560d042b
Bradley Ryan <>
2025-05-11 22:07:24 -0700

    Added width of Tag text sizes to prepare for actions

--- Garage/src/Utilities.h
+++ Garage/src/Utilities.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#define MAX_CHAR_VEHICLE_NAME 32
+#define MAX_CHAR_VEHICLE_NAME   32
+#define MAX_CHAR_TAG            32
+#define MAX_TAGS                4
 
 #define STRINGIFY(x) #x
 #define TO_STRING(x) STRINGIFY(x)
