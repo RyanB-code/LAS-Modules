85273e01a680eb4708f4dfa913976ae7ead4f868
Bradley Ryan <59941443+RyanB-code@users.noreply.github.com>
2025-07-19 16:49:25 -0700

    Added reading and writing of guns and amount of ammo. still testing setup though

--- Shooter-Central/src/Framework.cpp
+++ Shooter-Central/src/Framework.cpp
@@ -39,6 +39,24 @@ bool Framework::setup(const std::string& directory, std::shared_ptr<bool> setSho
     }
 
     shown = setShown;
+
+    // TESTING
+    if(!readGuns(paths.gunsDir))
+        log_error("Failed reading Guns");
+    if(!readAmmo(paths.ammoDir))
+        log_error("Failed reading Ammo");
+
+    for(auto itr {model.knownGuns_cbegin()}; itr != model.knownGuns_cend(); ++itr) {
+        const auto& [key, value] {*itr};
+        std::cout << std::format("  Name: {}, WT: {}, Cart: {}\n", value->name, value->weaponType.getName(), value->cartridge.getName());
+    }
+    for(auto itr {model.ammoStockpile_cbegin()}; itr != model.ammoStockpile_cend(); ++itr) {
+        const auto& [key, value] {*itr};
+        const AmmoMetadata& data {value.getAmountOfAmmo().getAmmo() };
+        std::cout << std::format("  Name: {}, Man: {}, Cart: {}, GW: {}, Amount: {}\n", data.name, data.manufacturer.getName(), data.cartridge.getName(), data.grainWeight, value.getAmountOfAmmo().getAmount());
+    }
+
+    // DONE TESTING
    
     log_info("SC Setup sucessful");
     return true;
@@ -74,6 +92,83 @@ void Framework::draw() {
 
     ImGui::End();
 }
+bool Framework::readGuns(const std::string& dir) {
+    if(!std::filesystem::exists(dir))
+        return false;
+
+    const std::filesystem::path workingDirectory{dir};
+	for(auto const& dirEntry : std::filesystem::directory_iterator(workingDirectory)){
+		try{
+            std::ifstream inputFile{ dirEntry.path(), std::ios::in };
+            json j = json::parse(inputFile);
+
+            GunMetadata gunBuf {j.template get<ShooterCentral::GunMetadata>()};
+
+            if(!model.knownGuns_add(std::make_shared<GunMetadata>(gunBuf))){
+                log_error("Could not insert GunMetadata from file [" + dirEntry.path().string() + ']');
+                continue;
+            }
+
+            if(!model.gunsInArmory_add(AssociatedGun{model.knownGuns_at(gunBuf)})){ 
+                log_error("Could not add AssociatedGun to stockpile from file [" + dirEntry.path().string() + ']');
+                continue;
+            }
+
+		}
+		catch(std::exception& e){
+            if(dirEntry.path().filename().string() != FILENAME_WEAPON_TYPES){  // Ignore the weaponTypes file
+                log_error("Failed to create GunMetadata object from file [" + dirEntry.path().string() + "]. What: " + std::string{e.what()});
+            }
+		}
+	}
+    
+	return true;
+}
+bool Framework::readAmmo(const std::string& dir) {
+    if(!std::filesystem::exists(dir))
+        return false;
+
+    const std::filesystem::path workingDirectory{dir};
+	for(auto const& dirEntry : std::filesystem::directory_iterator(workingDirectory)){
+		try{
+            std::ifstream inputFile{ dirEntry.path(), std::ios::in };
+            json j = json::parse(inputFile);
+
+            // Make the AmountOfAmmo object
+            // CANNOT be from_json due to not wanting to recast the shared_ptr in creating a new AmountOfAmmo object
+            std::string manufacturerBuf { }, cartridgeBuf { };
+            AmmoMetadata infoBuf { };
+
+            j.at("name").get_to(infoBuf.name);
+            j.at("manufacturer").get_to(manufacturerBuf);
+            j.at("cartridge").get_to(cartridgeBuf);
+            j.at("grain").get_to(infoBuf.grainWeight);
+
+            infoBuf.manufacturer   = Manufacturer{manufacturerBuf};
+            infoBuf.cartridge      = Cartridge{cartridgeBuf};
+
+            if(!model.knownAmmo_add(std::make_shared<AmmoMetadata>(infoBuf))){
+                log_error("Could not insert AmmoMetadata from file [" + dirEntry.path().string() + ']');
+                continue;
+            }
+
+            // Make the AmountOfAmmo object and add to stockpile
+            int amountBuf { 0 };
+            j.at("amount").get_to(amountBuf);
+            if(!model.ammoStockpile_add(AmountOfAmmo{model.knownAmmo_at(infoBuf), amountBuf})){ // Implicit conversion to AssociatedAmmo
+                log_error("Could not add AssociatedAmmo to stockpile from file [" + dirEntry.path().string() + ']');
+                continue;
+            }
+		}
+		catch(std::exception& e){
+            if(dirEntry.path().filename().string() != FILENAME_WEAPON_TYPES){  // Ignore the weaponTypes file
+                log_error("Failed to create GunMetadata object from file [" + dirEntry.path().string() + "]. What: " + std::string{e.what()});
+            }
+		}
+	}
+    
+	return true;
+}
 
 
 bool Setup::setupFilesystem(Framework::Filepaths& paths){
@@ -103,3 +198,146 @@ bool Setup::setupFilesystem(Framework::Filepaths& paths){
     return true;
 }
  
+
+namespace ShooterCentral::FileIO {
+
+std::string makeFileName    (std::string directory, const GunMetadata& gun) {
+    std::ostringstream fileName;
+    fileName << directory;
+
+    for(const auto& c : gun.weaponType.getName()){     // Remove spaces
+        if(isalnum(c))
+            fileName << c;
+        else if(c == ' ' || c == '\t')
+            fileName << '-';
+    }
+
+    fileName << '_';
+
+    for(const auto& c : gun.cartridge.getName()){     // Remove spaces
+        if(isalnum(c))
+            fileName << c;
+        else if(c == ' ' || c == '\t')
+            fileName << '-';
+    }
+
+    fileName << '_';
+
+    for(const auto& c : gun.name){     // Remove spaces
+        if(isalnum(c))
+            fileName << c;
+        else if(c == ' ' || c == '\t')
+            fileName << '-';
+    }
+
+    fileName << ".json";
+
+    return fileName.str();
+}
+std::string makeFileName (std::string directory, const AmmoMetadata& ammo) {
+    std::ostringstream fileName;
+    fileName << directory;
+
+    for(const auto& c : ammo.manufacturer.getName()){     // Remove spaces and make lowercase
+        if(isalnum(c))
+            fileName << c;
+        else if(c == ' ' || c == '\t')
+            fileName << '-';
+    }
+
+    fileName << '_';
+
+    for(const auto& c : ammo.name){     // Remove spaces and make lowercase
+        if(isalnum(c))
+            fileName << c;
+        else if(c == ' ' || c == '\t')
+            fileName << '-';
+    }
+
+    fileName << '_' << ammo.grainWeight;
+
+    fileName << ".json";
+
+    return fileName.str();
+}
+bool write(std::string directory, const GunMetadata& data) {
+    using LAS::json;
+
+    if(directory.empty())
+       return false;
+
+    directory = LAS::TextManip::ensureSlash(directory);
+
+    if(!std::filesystem::exists(directory))
+		return false;
+
+    json j = data;
+
+    // Write to file
+    std::ofstream file{ makeFileName(directory, data) };
+    file << std::setw(1) << std::setfill('\t') << j;
+    file.close();
+   
+    return true;
+}
+bool write(std::string directory, const AmountOfAmmo& data) {
+    using LAS::json;
+
+    if(directory.empty())
+       return false;
+
+    directory = LAS::TextManip::ensureSlash(directory);
+
+    if(!std::filesystem::exists(directory))
+		return false;
+
+
+    const AmmoMetadata& ammoInfo { data.getAmmo() }; // Throws if ptr is null inside AmountOfAmmo
+    json j;
+
+    j = LAS::json {
+        { "name",           ammoInfo.name },
+        { "manufacturer",   ammoInfo.manufacturer },
+        { "cartridge",      ammoInfo.cartridge },
+        { "grain",          ammoInfo.grainWeight},
+        { "amount",         data.getAmount() }
+    };
+
+    // Write to file
+    std::ofstream file{ makeFileName(directory, ammoInfo) };
+    file << std::setw(1) << std::setfill('\t') << j;
+    file.close();
+   
+    return true;
+}
+
+} // End FileIO namespace
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Shooter-Central/src/Framework.h
+++ Shooter-Central/src/Framework.h
@@ -6,6 +6,7 @@
 #include "Model.h"
 
 #include <LAS/Logging.h>
+#include <LAS/json.h>
 
 #include <string>
 #include <stdint.h>
@@ -44,9 +45,23 @@ namespace ShooterCentral{
         UnsavedChanges  unsavedChanges { };
 
         std::shared_ptr<bool> shown;
+
+        static constexpr char FILENAME_WEAPON_TYPES[] = "WeaponTypes.json";
+
+
+        bool readGuns(const std::string& dir);
+        bool readAmmo(const std::string& dir);
     };
 
     namespace Setup {
         bool    setupFilesystem(Framework::Filepaths& paths); // Needs parentDir set first
+    }
+
+    namespace FileIO {
+        std::string makeFileName    (std::string directory, const GunMetadata& data);
+        std::string makeFileName    (std::string directory, const AmmoMetadata& data);
+
+        bool write (std::string directory, const GunMetadata& data);
+        bool write (std::string directory, const AmountOfAmmo& data);
     }
 }
