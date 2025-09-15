a9dd2981a198482f74a38980909beb400fd4c626
Bradley Ryan <59941443+RyanB-code@users.noreply.github.com>
2025-09-14 21:00:50 -0700

    Screen handling changes
    Stores the last screen and compares the current screen. If different, and outside event changed it and need to force ImGui Tabs to update. I implemented this

--- Shooter-Central/src/View.cpp
+++ Shooter-Central/src/View.cpp
@@ -9,25 +9,58 @@ UIController::~UIController() {
 
 }
 void UIController::draw(const Containers& containers, const UnsavedChanges& unsavedChanges) {
-     if(ImGui::BeginTabBar("Tabs")){
-        if(ImGui::BeginTabItem("Home")){
+    // Stores last screen, checks if equal to current screen (set after ImGui does its thing)
+    // If not equal, it means an Event changed the screen, so force ImGui to show which tab we want
+    // from the Event
+
+    static Screen lastScreen;
+    ImGuiTabItemFlags homeFlags { 0 }, viewFlags { 0 }, addFlags { 0 }, editFlags { 0 };
+    bool applyForce { false };
+
+    if(lastScreen != currentScreen)
+        applyForce = true;
+
+    if(applyForce){
+        switch(currentScreen){
+            case Screen::HOME:
+                homeFlags |= ImGuiTabItemFlags_SetSelected;
+                break;
+            case Screen::VIEW:
+                viewFlags |= ImGuiTabItemFlags_SetSelected;
+                break;
+            case Screen::ADD:
+                addFlags |= ImGuiTabItemFlags_SetSelected;
+                break;
+            case Screen::EDIT:
+                editFlags |= ImGuiTabItemFlags_SetSelected;
+                break;
+            default:
+                LAS::log_warn("SC Screen case not handled");
+                break;
+        }
+    }
+        
+    if(ImGui::BeginTabBar("Tabs")){
+        if(ImGui::BeginTabItem("Home", nullptr, homeFlags)){
             currentScreen = Screen::HOME;
             ImGui::EndTabItem();
         }
-        if(ImGui::BeginTabItem("View")){
+        if(ImGui::BeginTabItem("View", nullptr, viewFlags)){
             currentScreen = Screen::VIEW;
             ImGui::EndTabItem();
         }
-        if(ImGui::BeginTabItem("Add")){
+        if(ImGui::BeginTabItem("Add", nullptr, addFlags)){
             currentScreen = Screen::ADD;
             ImGui::EndTabItem();
         }
-        if(ImGui::BeginTabItem("Edit")){
+        if(ImGui::BeginTabItem("Edit", nullptr, editFlags)){
             currentScreen = Screen::EDIT;
             ImGui::EndTabItem();
         }
         ImGui::EndTabBar();
     }
+
+    lastScreen = currentScreen;
 
     switch(currentScreen){
         case Screen::HOME:
