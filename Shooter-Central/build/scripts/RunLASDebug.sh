b6ada417364875aec120051d45d861634d76bba5
Bradley Ryan <59941443+RyanB-code@users.noreply.github.com>
2025-10-10 11:24:52 -0700

    Modified build scripts
    Removed las-directory.dat and made a separate Config.cfg file to configure the scripts. Added better instructions in the config file. Updated build scripts accordingly

--- Shooter-Central/build/scripts/RunLASDebug.sh
+++ Shooter-Central/build/scripts/RunLASDebug.sh
@@ -3,15 +3,15 @@
 # Input LAS parent directory location into the file
 # with a backslash at the end
 # DO NOT change the variable in this script unless that file name changes
-LAS_LOCATION_FILE=las-directory.dat
+CONFIG_FILENAME=Config.cfg
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )     # Where script is (should be Garage/build/scripts/
-CONFIG_FILE="${SCRIPT_DIR}/${LAS_LOCATION_FILE}"
+CONFIG_PATH="${SCRIPT_DIR}/${CONFIG_FILENAME}"
 
-if [[ -e ${CONFIG_FILE} ]]; then
-    source "${CONFIG_FILE}"
+if [[ -e ${CONFIG_PATH} ]]; then
+    source "${CONFIG_PATH}"
 else
-    echo "Configuration file not found at expected location: ${CONFIG_FILE}"
+    echo "Configuration file not found at expected location: ${CONFIG_PATH}"
     exit 1
 fi
 
